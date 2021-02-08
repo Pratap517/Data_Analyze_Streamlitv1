8ffc1a539e06d36868ccf125d189576d13f2fa1e
Prathap reddy K <65093897+Pratap517@users.noreply.github.com>
2021-02-08 13:25:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ def main():
                   """, unsafe_allow_html=True)
                   
         st.subheader("Please Select the type of report in Menu")
-	st.markdown('  **_Note:_**. :smile:')
+	st.markdown('  **_Note-_**.:smile:')
 	st.write(" if in case page crashed, Request you to reload page and try using csv file with less features and size upto 5 Mb.")
 	
 	
